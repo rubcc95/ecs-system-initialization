c321c1a4c21e88873862db25e02e219449cfbe79
rubcc95 <rubcc95@gmail.com>
2023-05-15 19:53:46 +0200

    Create SystemSettings.cs

---
+++ Src/SystemSettings.cs
@@ -0,0 +1,32 @@
+#nullable enable
+
+using System;
+using UnityEngine;
+
+
+[Serializable]
+public struct SystemSettings
+{
+    public SystemSettings(Type type)
+    {
+        _name = type.Name;
+        _assemblyQualifiedName = type.AssemblyQualifiedName;
+        _active = true;
+    }
+
+    [SerializeField] string _name;
+    [SerializeField] string _assemblyQualifiedName;
+
+    [SerializeField] bool _active;
+
+    public string AssemblyQualifiedName => _assemblyQualifiedName;
+    public bool Active
+    {
+        get => _active;
+        set => _active = value;
+    }
+
+
+    public static implicit operator Type(SystemSettings type) => Type.GetType(type._name);
+    public static implicit operator SystemSettings(Type type) => new(type);
+}
