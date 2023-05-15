4c31b8388d2ee52381e23351cf4e307c6e6d49e6
rubcc95 <rubcc95@gmail.com>
2023-05-15 20:03:41 +0200

    Update SystemInitializationSettings.cs

--- Src/SystemInitializationSettings.cs
+++ Src/SystemInitializationSettings.cs
@@ -5,52 +5,55 @@ using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Entities/System Initialization Settings")]
-public class SystemInitializationSettings : ScriptableObject, ISerializationCallbackReceiver
+namespace Hagans.Ecs.SystemInitialization
 {
-    [SerializeField] SystemSettings[] _settings = new SystemSettings[0];
-
-    public IReadOnlyList<SystemSettings> Settings => _settings;
-
-    public static SystemInitializationSettings? Instance { get; private set; }
-
-    public IReadOnlyList<Type> Systems { get; private set; } = new Type[0];
- 
-    public void OnAfterDeserialize()
+    [CreateAssetMenu(menuName = "Entities/System Initialization Settings")]
+    public class SystemInitializationSettings : ScriptableObject, ISerializationCallbackReceiver
     {
-        Instance = this;
+        [SerializeField] SystemSettings[] _settings = new SystemSettings[0];
 
-        var systems = new List<Type>();
+        public IReadOnlyList<SystemSettings> Settings => _settings;
 
-        for (int i = 0; i < _settings.Length; i++)
-            if (_settings[i].Active)
-                systems.Add(_settings[i]);
+        public static SystemInitializationSettings? Instance { get; private set; }
 
-        Systems = systems;
+        public IReadOnlyList<Type> Systems { get; private set; } = new Type[0];
+
+        public void OnAfterDeserialize()
+        {
+            Instance = this;
+
+            var systems = new List<Type>();
+
+            for (int i = 0; i < _settings.Length; i++)
+                if (_settings[i].Active)
+                    systems.Add(_settings[i]);
+
+            Systems = systems;
+        }
+
+        public void OnBeforeSerialize()
+        {                
+            var allSystems = TypeManager.GetSystems(WorldSystemFilterFlags.Default);
+
+            var newSettings = new SystemSettings[allSystems.Count];
+
+            for (int i = 0; i < newSettings.Length; i++)        
+                newSettings[i] = Contains(allSystems[i], out var settings) ? settings : new(allSystems[i]);        
+
+            _settings = newSettings;
+        }
+
+        bool Contains(Type type, out SystemSettings settings)
+        {
+            for (int i = 0; i < _settings.Length; i++)
+                if (_settings[i].AssemblyQualifiedName == type.AssemblyQualifiedName)
+                {
+                    settings = _settings[i];
+                    return true;
+                }
+
+            settings = default;
+            return false;
+        }  
     }
-
-    public void OnBeforeSerialize()
-    {                
-        var allSystems = TypeManager.GetSystems(WorldSystemFilterFlags.Default);
-
-        var newSettings = new SystemSettings[allSystems.Count];
-
-        for (int i = 0; i < newSettings.Length; i++)        
-            newSettings[i] = Contains(allSystems[i], out var settings) ? settings : new(allSystems[i]);        
-        
-        _settings = newSettings;
-    }
-
-    bool Contains(Type type, out SystemSettings settings)
-    {
-        for (int i = 0; i < _settings.Length; i++)
-            if (_settings[i].AssemblyQualifiedName == type.AssemblyQualifiedName)
-            {
-                settings = _settings[i];
-                return true;
-            }
-
-        settings = default;
-        return false;
-    }  
 }
