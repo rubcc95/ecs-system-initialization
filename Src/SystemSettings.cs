50fa5ac79dfe14ca76c5fb2625069297c2e64908
rubcc95 <rubcc95@gmail.com>
2023-05-15 20:04:11 +0200

    Update SystemSettings.cs

--- Src/SystemSettings.cs
+++ Src/SystemSettings.cs
@@ -3,30 +3,32 @@
 using System;
 using UnityEngine;
 
-
-[Serializable]
-public struct SystemSettings
+namespace Hagans.Ecs.SystemInitialization
 {
-    public SystemSettings(Type type)
+    [Serializable]
+    public struct SystemSettings
     {
-        _name = type.Name;
-        _assemblyQualifiedName = type.AssemblyQualifiedName;
-        _active = true;
+        public SystemSettings(Type type)
+        {
+            _name = type.Name;
+            _assemblyQualifiedName = type.AssemblyQualifiedName;
+            _active = true;
+        }
+
+        [SerializeField] string _name;
+        [SerializeField] string _assemblyQualifiedName;
+
+        [SerializeField] bool _active;
+
+        public string AssemblyQualifiedName => _assemblyQualifiedName;
+        public bool Active
+        {
+            get => _active;
+            set => _active = value;
+        }
+
+
+        public static implicit operator Type(SystemSettings type) => Type.GetType(type._name);
+        public static implicit operator SystemSettings(Type type) => new(type);
     }
-
-    [SerializeField] string _name;
-    [SerializeField] string _assemblyQualifiedName;
-
-    [SerializeField] bool _active;
-
-    public string AssemblyQualifiedName => _assemblyQualifiedName;
-    public bool Active
-    {
-        get => _active;
-        set => _active = value;
-    }
-
-
-    public static implicit operator Type(SystemSettings type) => Type.GetType(type._name);
-    public static implicit operator SystemSettings(Type type) => new(type);
 }
